ef6ebf9d2435995142867d15a8f25ee54660bc54
Jaymax-cyber <jossycode0@gmail.com>
2026-07-30 13:00:16 +0100

    updt

--- component/Input.tsx
+++ component/Input.tsx
@@ -7,7 +7,7 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 const Input: React.FC<InputProps> = ({ className, ...props }) => {
   return (
     <input
-      className={`shadow appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${className}`}
+      className={`shadow appearance-none border border-gray-400 rounded w-full py-2 px-3 text-gray-900 leading-tight focus:outline-none focus:shadow-outline ${className}`}
       {...props}
     />
   );
